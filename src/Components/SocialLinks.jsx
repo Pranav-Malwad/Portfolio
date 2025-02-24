41a7491b1509877517ee44a1b1d0f427101a956b
Pranav Malwad <129570487+Pranav-Malwad@users.noreply.github.com>
2025-02-24 14:54:52 +0530

    Update SocialLinks.jsx

--- src/Components/SocialLinks.jsx
+++ src/Components/SocialLinks.jsx
@@ -39,7 +39,7 @@ const SocialLinks = () => {
           Resume <BsFillPersonLinesFill size={30}></BsFillPersonLinesFill>
         </>
       ),
-      href: "/resume.pdf",
+      href: "/pranav malwad resume.pdf",
       style: "rounded-br-md",
       download: true,
     },
